9d11326228027ed1bd30afd3b3168233ca5dd522
Sumit Man <summitmman@gmail.com>
2025-05-01 12:40:39 +0200

    add json field to articles

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -451,6 +451,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
         maxLength: 80;
       }>;
     locale: Schema.Attribute.String;
+    locales: Schema.Attribute.JSON;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
       'api::article.article'
